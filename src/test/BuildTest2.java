0f6486cc39804a3988e142a565320116ba210e3c
falius on ideapad-ubuntu <ciero639@gmail.com>
2017-02-28 20:38:24 +0900

    gradle eclipseを実行

---
+++ src/test/BuildTest2.java
@@ -0,0 +1,12 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BuildTest2 {
+
+    @Test
+    public void failedTest() {
+        assertTrue(false);
+    }
+}
